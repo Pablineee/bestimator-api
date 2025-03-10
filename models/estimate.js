bfdf5edd3b090569d545a052b5c63173569ae347
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-03-10 14:20:54 -0400

    Updated status enum in Estimate model, as well as removed the Estimate model's materials property

--- models/estimate.js
+++ models/estimate.js
@@ -1,12 +1,66 @@
-// Using external status enum in case it is required
-// in the Estimate controller's business logic
-const status = require('../config/status');
+const estimateStatus = require('../config/estimateStatus');
 
 const Estimate = (sequelize, DataTypes) => sequelize.define('Estimate', {
     estimate_id: {
         type: DataTypes.STRING,
         primaryKey: true,
     },
+    user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: "User",
+            key: "user_id",
+        },
+    },
+    client_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: "Client",
+            key: "client_id",
+        },
+    },
+    job_type_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "JobType",
+            key: "job_type_id",
+        },
+    },
+    province_weight_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "ProvinceWeight",
+            key: "province_weight_id",
+        },
+    },
+    costs: {
+        type: DataTypes.JSON,
+        allowNull: false,
+    },
+    additional_costs: {
+        type: DataTypes.JSON,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM(...Object.values(estimateStatus)),
+        allowNull: false,
+        defaultValue: estimateStatus.DRAFT,
+    },
+    notes: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    valid_until: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    total_cost: {
+        type: DataTypes.DECIMAL(10, 2)
+    }
 });
 
 module.exports = Estimate;
