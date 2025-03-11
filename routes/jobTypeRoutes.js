61da0cf995a1c8981c4b79af12dbd68c2d3c8766
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-03-11 17:23:58 -0400

    JobType and Unit routes implemented, with the use of their respective controllers

--- routes/jobTypeRoutes.js
+++ routes/jobTypeRoutes.js
@@ -1,5 +1,63 @@
 const express = require('express');
 const router = express.Router();
-// Import necessary model
+const { getJobTypes, addJobType, updateJobType, deleteJobType } = require('../controllers/jobTypeController');
+
+// Get all Job Type
+router.get('/', async (req, res) => {
+    try {
+        const jobTypes = await getJobTypes();
+        return jobTypes.length > 0 ? res.status(200).json({ jobTypes }) : res.status(200).json({});
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Create new Job Type
+router.post('/', async (req, res) => {
+    try {
+        const { job_type } = req.body;
+        const newJobType = await addJobType(job_type);
+        return res.status(201).json({
+            message: 'New Job Type added successfully',
+            data: newJobType,
+        });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Update existing Job Type
+router.put('/', async (req, res) => {
+    try {
+        const { job_type_id, job_type } = req.body;
+        const updatedJobType = await updateJobType(job_type_id, job_type);
+        if (updatedJobType){
+            return res.status(200).json({
+                message: 'Job Type updated successfully',
+            });
+        }
+        return res.status(404).json({ error: 'Job Type not found' });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Delete existing Job Type
+router.delete('/', async (req, res) => {
+    try {
+        const { job_type_id } = req.body;
+        const jobTypeDeleted = await deleteJobType(job_type_id);
+        if (jobTypeDeleted){
+            return res.status(204).send();
+        }
+        return res.status(404).json({ error: 'Job Type not found' });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
--- routes/unitRoutes.js
+++ routes/unitRoutes.js
@@ -1,5 +1,63 @@
 const express = require('express');
 const router = express.Router();
-// Import necessary model
+const { getUnits, addUnit, updateUnit, deleteUnit } = require('../controllers/unitController');
+
+// Get all Unit
+router.get('/', async (req, res) => {
+    try {
+        const units = await getUnits();
+        return units.length > 0 ? res.status(200).json({ units }) : res.status(200).json({});
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Create new Unit
+router.post('/', async (req, res) => {
+    try {
+        const { unit_name } = req.body;
+        const newUnit = await addUnit(unit_name);
+        return res.status(201).json({
+            message: 'New Unit added successfully',
+            data: newUnit,
+        });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Update existing Unit
+router.put('/', async (req, res) => {
+    try {
+        const { unit_id, unit_name } = req.body;
+        const updatedUnit = await updateUnit(unit_id, unit_name);
+        if (updatedUnit){
+            return res.status(200).json({
+                message: 'Unit updated successfully',
+            });
+        }
+        return res.status(404).json({ error: 'Unit not found' });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Delete existing Unit
+router.delete('/', async (req, res) => {
+    try {
+        const { unit_id } = req.body;
+        const unitDeleted = await deleteUnit(unit_id);
+        if (unitDeleted){
+            return res.status(204).send();
+        }
+        return res.status(404).json({ error: 'Unit not found' });
+    } catch(err){
+        console.error(`Error: ${err.message}`);
+        return res.status(501).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
