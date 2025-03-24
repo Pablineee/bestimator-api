dc3873b4315bbd19cc21694a449014b61cbe306d
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-03-24 12:03:12 -0400

    Updated estimateController getEstimateById function to also return Materials directly

--- controllers/estimateController.js
+++ controllers/estimateController.js
@@ -30,8 +30,11 @@ const getEstimateById = async (estimateId) => {
             { model: JobType, attributes: ['job_type'] },
             { model: ProvinceWeight, attributes: ['province', 'province_weight', 'province_tax_rate'] },
             { 
-                model: EstimateMaterial,
-                include: [{ model: Material, attributes: ['name', 'price', 'unit_id', 'coverage'] }]
+                model: Material,
+                attributes: ['name', 'price', 'unit_id', 'coverage'],
+                through: {
+                    attributes: ['quantity', 'initial_unit_cost', 'total_cost']
+                }
             }
         ]
     });
