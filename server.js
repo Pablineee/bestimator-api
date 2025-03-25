b0d65b6b8707f5b34b83c3d32185835720517c6a
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-03-25 13:31:40 -0400

    Minor refactoring of server.js, to improve readability and remove unnecessary code

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const provinceWeightRoutes = require('./routes/provinceWeightRoutes');
 const swaggerRoutes = require('./routes/swaggerRoutes');
 
 // Import Clerk authentication utility
-const { clerkAuth, clerkMiddleware } = require('./utils/clerkAuth');
+const { clerkAuth } = require('./utils/clerkAuth');
 
 // Initialize Express app
 const app = express();
@@ -28,14 +28,7 @@ app.use(express.json());
 // app.use(clerkMiddleware());
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 
-// Define API routes (Unprotected)
-// app.use('/v1/materials', materialRoutes);
-// app.use('/v1/users', userRoutes);
-// app.use('/v1/clients', clientRoutes);
-// app.use('/v1/estimates', estimateRoutes);
-// app.use('/v1/units', unitRoutes);
-// app.use('/v1/job-types', jobTypeRoutes);
-// app.use('/v1/province-weights', provinceWeightRoutes);
+// Unprotected API routes
 app.use('/api-docs', swaggerRoutes);
 
 // Protected API routes - For use with Clerk authorization headers
@@ -46,7 +39,6 @@ app.use('/v1/estimates', clerkAuth, estimateRoutes);
 app.use('/v1/units', clerkAuth, unitRoutes);
 app.use('/v1/job-types', clerkAuth, jobTypeRoutes);
 app.use('/v1/province-weights', clerkAuth, provinceWeightRoutes);
-// app.use('/api-docs', clerkAuth, swaggerRoutes);
 
 // Set necessary environment variables
 const SERVER_PORT = process.env.PORT || 4000;
@@ -57,9 +49,9 @@ const startServer = async () => {
         await connectDB(); // Connect to PostgreSQL database instance
         console.log("Database Synced Successfully");
 
-        app.listen(SERVER_PORT, '0.0.0.0', () =>
-            console.log(`Server running on http://0.0.0.0:${SERVER_PORT}`)
-        );
+        app.listen(SERVER_PORT, '0.0.0.0', () => {
+            console.log(`Server running on http://0.0.0.0:${SERVER_PORT}`);
+        });
     } catch (err) {
         console.error(`Database Sync Error: ${err.message}`);
         process.exit(1); // Exit on database sync failure
@@ -68,16 +60,3 @@ const startServer = async () => {
 
 // Start the server
 startServer();
-
-// ---------- Testing Purposes -------------
-
-// Request is successful (no authentication)
-app.get('/test', (req, res) => {
-    return res.json({ message: 'Success' });
-});
-
-// Endpoint uses Clerk Authentication
-// Will return error, unless Clerk authorization headers are included in API request
-app.get('/test-auth', clerkAuth, (req, res) => {
-    return res.json({ message: 'Success' });
-});
